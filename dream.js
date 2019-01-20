bf6417a2e40a6a7f5653527a143c291244b58410
Mr_3moreee <45393804+mr3moreee@users.noreply.github.com>
2019-01-20 12:14:29 +0200

    Create dream.js

---
+++ dream.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Spaming :>`,"http://twitch.tv/Dream")
+client.user.setStatus("#IT WORKS!!")
+});
+
+
+
+
+
+
+
+
+
+
+client.login("MzU5MDk4NTUzNTI5Nzk0NTcy.Dtqtrg.36eaS4mRBWeALLZqZqK2uhCGCq0");// لا تغير فيها شيء
