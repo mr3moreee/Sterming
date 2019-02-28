87b01575aa85146efdf21f76cad2bd6c169a97d6
Mr_3moreee <45393804+mr3moreee@users.noreply.github.com>
2019-02-28 11:45:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
- 
+var prefix = "+";
 client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame("الستريم هنا", "https://www.twitch.tv/Alpha");
+client.user.setGame(`مذاكرة`,"http://twitch.tv/Dream")
+client.user.setStatus("#IT WORKS!!")
 });
+
+
+
+client.login("NDIyNzI5MjU1ODI3Mjc1Nzc2.D1gg1w.hZxgucitIZbtQ-ajQaarvjom17A");// لا تغير فيها شيء
